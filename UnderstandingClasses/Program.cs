8595b366c4bd6c5d17bf5405865a115daee0154a
Charlie Cuddy <charliecuddy4@gmail.com>
2017-01-04 01:19:39 -0600

    Add project files.

---
+++ UnderstandingClasses/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Dodge";
+            myCar.Model = "Neon";
+            myCar.Year = 1997;
+            myCar.Color = "White";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+            Console.ReadLine();
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 650M; //just make up a price
+            return carValue;
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 2000)
+                carValue = 10000;
+            else
+                carValue = 2000;
+            return carValue;
+        }
+    }
+
+
+
+}
